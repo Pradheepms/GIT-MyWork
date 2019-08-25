6137f382ce99d7379c47ac59988562c63190308c
Pradheepms <36770286+Pradheepms@users.noreply.github.com>
2019-08-25 16:35:00 +0530

    Initial Commit

---
+++ UpdateConnectionStrings/UpdateConnectionStrings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UpdateConnectionStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
